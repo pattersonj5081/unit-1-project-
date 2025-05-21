8c36f8f8129638d5c22dc4cfa76752f96c83ab7d
pattersonj5081 <150544502+pattersonj5081@users.noreply.github.com>
2025-05-21 13:38:58 -0500

    initial commit. Initial HTML and CSS for vis has been set. Starting on components.

---
+++ Unit-1-Final-Proj/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { createElement } from 'react';
+import './App.css';
+import Loader from './components/loader';
+
+
+function App() {
+ return (
+  <>
+    <div className="App">
+    <Loader/>
+    </div>
+  </>
+ );
+}
+
+export default App;
